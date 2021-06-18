80b666a66752ff0a7c7e5bf3476dc91c9c173a14
snigdhaguha22 <86109988+snigdhaguha22@users.noreply.github.com>
2021-06-18 23:16:35 +0530

    Add files via upload

---
+++ Detailed solution.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 18 23:12:49 2021
+
+@author: jrana1
+"""
+
+Problem Statement:
+There is no denying that air pollution is impacting nature and the climate. 
+An Air Quality index value above 100 translates to very unhealthy air quality conditions for survival, 
+with high levels of health concern. We are working on a "Air Quality Index Prediction" model 
+using Webscrapping and Machine learning/ Deep learning concepts to predict the air pollution 
+index based on the historical data provided to help the government to administer the same.
+
+Solution :
+    Developed a Air Quality Index Prediciton Model by using Webscrapping and Machine Learning and
+    Deep Learning concept.
+    Data collection from the website - http://en.tutiempo.net/climate
+    
+Trained our model in below Algorithms. Here we see that "RandomForestRegressor" is giving
+us the best accuracy. so for our model this algorithm is the best.
+
+Linear Regression = 0.4710569304807393
+
+KNNRegressor = 0.39387489016756577
+
+RandomForestRegressor = 0.7078763563047866
+
+KNeighborsRegressor = 0.39387489016756577
+
+XgboostRegressor = 0.6582827367763413
+
